f033739547cac444d0261e02168aea182d20cfcb
Somo S. <5506400+somombo@users.noreply.github.com>
2025-11-11 13:38:20 +0000

    fix pointing to url directly not working
    Signed-off-by: Somo S. <5506400+somombo@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+GEMINI_CLI_VERSION=v0.13.0
 echo "GEMINI_CLI_NO_RELAUNCH=true" > .env
-rm -rf ./bin/*
-mkdir -p ./bin
-# DENO_COMPAT=1 deno compile -A --env-file=.env --no-check -o bin/gemini npm:@google/gemini-cli
-DENO_COMPAT=1 deno compile -A --env-file=.env --no-check -o ./bin/gemini https://github.com/google-gemini/gemini-cli/releases/download/v0.13.0/gemini.js
+OUT_DIR=./dist
+rm -rf $OUT_DIR/*
+mkdir -p $OUT_DIR
+# DENO_COMPAT=1 deno compile -A --env-file=.env --no-check --output=$OUT_DIR npm:@google/gemini-cli
+# DENO_COMPAT=1 deno compile -A --env-file=.env --no-check --output=$OUT_DIR https://github.com/google-gemini/gemini-cli/releases/download/v0.13.0/gemini.js
+
+wget https://github.com/google-gemini/gemini-cli/releases/download/$GEMINI_CLI_VERSION/gemini.js -O $OUT_DIR/gemini.js
+
+export DENO_COMPAT=1 
+deno compile -A --env-file=.env --no-check --output=$OUT_DIR/gemini  $OUT_DIR/gemini.js
